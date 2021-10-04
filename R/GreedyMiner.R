82d50e24ed2e96f195bee5db0064b0478afb254f
officialprofile <75314157+officialprofile@users.noreply.github.com>
2021-10-04 08:41:29 +0200

    Add files via upload

--- R/GreedyMiner.R
+++ R/GreedyMiner.R
@@ -1,28 +1,29 @@
 # qual.na.action = 'drop', 'mode', 'ignore'
 # quant.na.action = 'drop', 'mean', 'median' 'ignore'
 # all.types; 0 - qualitative, 1 - quantitative, e.g. c(0,1,1,1,0,0,1)
-# dependent.type; 0 - qualitative, 1 - quantitative
 
 GreedyMiner <- function(dataset,
-                        dependent.vars = NULL,
-                        dependent.type = 0,
                         all.types = NULL,
                         qual.na.action = 'mean',
                         quant.na.action = 'mode') {
 
   cols <- colnames(dataset)
 
-  # Determine types of columns
+  results <- list('ANOVA' = c(), 'Corr Pearson' = c(), 'Corr Spearman' = c())
+  anova_results    <- c()
+  pearson_results  <- c()
+  spearman_results <- c()
+
+  # Determine types of the columns
   if (is.null(all.types)){
-    print('Argument all.types is not specified. Types will be determined automatically')
-    auto_types <- sapply(dataset, typeof)
+    warning('Argument all.types is not specified. Types will be determined automatically\n')
+    auto_types <- sapply(dataset, class)
     ql <- rep(0, ncol(dataset))
     qt <- rep(0, ncol(dataset))
     for (i in 1:ncol(dataset)){
-      if (auto_types[i] %in% c('integer', 'double')){
+      if (auto_types[[i]][1] == 'numeric'){
         qt[i] <- 1
-      }
-      if (auto_types[i] %in% c('character', 'factor')){
+      } else {
         ql[i] <- 1
       }
     }
@@ -34,7 +35,7 @@ GreedyMiner <- function(dataset,
     qt   <- cols[which(all.types == 1)]
   }
 
-  # Convert qualitative variable to factor
+  # Convert qualitative variables to factors
   for (var in ql){
     dataset[, var] = factor(dataset[, var])
   }
@@ -75,36 +76,46 @@ GreedyMiner <- function(dataset,
   #   }
   # }
 
-
-
   # Check correlation for every quantitative variable
-
+  options(warn = -1)
   for (qt1 in qt){
     for (qt2 in qt){
       if (qt1 != qt2){
-        ct <- cor.test(dataset[,qt1], dataset[,qt2])
-        if (ct$p.value < 0.05){
-          print(paste('Correlation: ', qt1, qt2, ct$estimate))
+        ct_p <- cor.test(dataset[,qt1], dataset[,qt2], method = "pearson")
+        ct_s <- cor.test(dataset[,qt1], dataset[,qt2], method = "spearman")
+        if (ct_p$p.value < 0.05){
+          pearson_results <- c(pearson_results,
+                               paste(qt1, qt2, ', cor = ', round(ct_p$estimate, 3),
+                                     ', p-val = ', round(ct_p$p.value, 3)))
+        }
+        if (ct_s$p.value < 0.05){
+          spearman_results <- c(spearman_results,
+                                paste(qt1, qt2, 'cor = ', round(ct_s$estimate, 3),
+                                      ', p-val = ', round(ct_p$p.value, 3)))
         }
       }
     }
   }
+  options(warn = getOption("warn"))
 
+  # Check differences between groups
   for (ql_var in ql){
     for (qt_var in qt){
       anova_test <- aov(as.formula(paste(qt_var, ql_var, sep = '~')), data = dataset)
       anova_summary <- summary(anova_test)
       p_vals <- na.omit(anova_summary[[1]]$`Pr(>F)`)
       if (min(p_vals) < 0.05){
-        print(paste('ANOVA: ', ql_var, qt_var, min(p_vals)))
+        anova_results <- c(anova_results, paste(ql_var, qt_var, ', p-val = ', round(min(p_vals), 3)))
       }
     }
   }
 
+  results$ANOVA           <- anova_results
+  results$`Corr Pearson`  <- pearson_results
+  results$`Corr Spearman` <- spearman_results
+
+  return(results)
 }
 
-# tit = GreedyMiner(dataset, all.types <- c(0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0))
-dataset = as.data.frame(readxl::read_excel('Pacjenci.xlsx'))
-GreedyMiner(dataset)
-
-
+GreedyMiner(esoph)
+GreedyMiner(ToothGrowth)
